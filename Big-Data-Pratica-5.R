49861f162c08c0b8231c2f7f442b8354f61a4d01
Alex Paulo Pereira Silva <alex.paulo100@gmail.com>
2023-01-23 19:41:25 -0300

    Update Big-Data-Pratica-5.R

--- Big-Data-Pratica-5.R
+++ Big-Data-Pratica-5.R
@@ -1,11 +1,5 @@
 # Big Data na Prática - Análise de Séries Temporais no Mercado Financeiro 
 
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
-# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
-
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-# Não use diretórios com espaço no nome
 setwd("C:/BigDataRAzure/projetosR/Big-Data-Pratica-5")
 getwd()
 
